a22fbf0df5e56fd0cb994b734d3e089668985446
Julio Martins de Sousa <juliomartinssousa@gmail.com>
2020-01-24 21:39:17 -0300

    Add widgets folder

---
+++ lib/widgets/audio_player.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AudioPlayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      height: 50,
+      child: Text('player'),
+    );
+  }
+}
